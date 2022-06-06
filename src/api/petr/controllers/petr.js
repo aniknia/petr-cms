a5dd8f3e7023819f75a2f54932e7c3a2076017d2
ari <aniknia.dev@gmail.com>
2022-06-05 18:04:59 -0700

    feat: add basic petr schema

---
+++ src/api/petr/controllers/petr.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  petr controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::petr.petr');
---
+++ src/api/petr/routes/petr.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * petr router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::petr.petr');
