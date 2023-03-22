85ba46c1c53f41353c3290a680d68ab2d1e8d22e
ari <aniknia.dev@gmail.com>
2023-03-22 15:47:33 -0700

    build: update strapi to 4.8.2

--- config/plugins.js
+++ config/plugins.js
@@ -17,5 +17,18 @@ module.exports = ({ env }) => ({
           delete: {},
         },
       },
+	},
+	email: {
+      config: {
+        provider: 'sendgrid', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+        providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'no-reply@petrarchive.com',
+        defaultReplyTo: 'help@petrarchive.com',
+        testAddress: 'test@petrarchive.com',
+      },
     },
-  });
+  },
+});
